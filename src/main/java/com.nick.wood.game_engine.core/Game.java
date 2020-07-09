9836ee7739901eebb221f33f46ae2d1a1f657bb1
Nickolas Wood <53903568+Qualia91@users.noreply.github.com>
2020-07-09 14:15:33 +0100

    at least its shutting down now

---
+++ src/main/java/com.nick.wood.game_engine.core/Game.java
@@ -0,0 +1,4 @@
+package com.nick.wood.game_engine.core;
+
+public class Game {
+}
--- src/main/java/com.nick.wood.game_engine.core/GameLoop.java
+++ src/main/java/com.nick.wood.game_engine.core/GameLoop.java
@@ -1,18 +1,19 @@
 package com.nick.wood.game_engine.core;
 
 import com.nick.wood.game_engine.event_bus.busses.GameBus;
+import com.nick.wood.game_engine.event_bus.event_types.ErrorEventType;
+import com.nick.wood.game_engine.event_bus.event_types.ManagementEventType;
 import com.nick.wood.game_engine.event_bus.event_types.RenderEventType;
+import com.nick.wood.game_engine.event_bus.events.ErrorEvent;
+import com.nick.wood.game_engine.event_bus.events.ManagementEvent;
 import com.nick.wood.game_engine.event_bus.events.RenderEvent;
 import com.nick.wood.game_engine.event_bus.interfaces.Bus;
 import com.nick.wood.game_engine.event_bus.interfaces.Event;
+import com.nick.wood.game_engine.event_bus.interfaces.EventType;
 import com.nick.wood.game_engine.event_bus.interfaces.Subscribable;
 import com.nick.wood.game_engine.model.game_objects.GameObject;
 import com.nick.wood.game_engine.model.input.ControllerState;
-import com.nick.wood.game_engine.model.input.DirectTransformController;
-import com.nick.wood.game_engine.model.input.GameManagementInputController;
-import com.nick.wood.game_engine.systems.TerrainGeneration;
-import com.nick.wood.game_engine.systems.GESystem;
-import com.nick.wood.graphics_library.Picking;
+import com.nick.wood.game_engine.systems.*;
 import com.nick.wood.graphics_library.RenderEventData;
 import com.nick.wood.graphics_library.Window;
 import com.nick.wood.graphics_library.WindowInitialisationParameters;
@@ -20,51 +21,49 @@ import com.nick.wood.graphics_library.objects.render_scene.RenderGraph;
 import com.nick.wood.graphics_library.objects.render_scene.Scene;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class GameLoop {
+public class GameLoop implements Subscribable {
+
+	private final Set<Class<?>> supports = new HashSet<>();
 
 	private static final float FPS = 60;
-	private volatile boolean shutdown = false;
 	private final WindowInitialisationParameters wip;
-	private final DirectTransformController directTransformController;
 	private final HashMap<String, ArrayList<GameObject>> layeredGameObjectsMap;
 	private final RenderingConversion renderingConversion;
 	private final GameBus gameBus;
 	private final ControllerState controllerState;
 	private final ExecutorService executorService;
-	private final GameManagementInputController gameManagementInputController;
 	private final ArrayList<Scene> sceneLayers;
 
 	private final ArrayList<GESystem> geSystems = new ArrayList<>();
+
+	private volatile boolean shutdown = false;
 
 	public GameLoop(ArrayList<Scene> sceneLayers,
 	                WindowInitialisationParameters wip,
 	                DirectTransformController directTransformController,
 	                HashMap<String, ArrayList<GameObject>> layeredGameObjectsMap) {
 
+		supports.add(ManagementEvent.class);
+
 		this.executorService = Executors.newFixedThreadPool(4);
+		this.gameBus = new GameBus();
 		this.sceneLayers = sceneLayers;
 		this.wip = wip;
 		this.layeredGameObjectsMap = layeredGameObjectsMap;
-		this.gameBus = new GameBus();
-
-		this.directTransformController = directTransformController;
 
 		this.renderingConversion = new RenderingConversion();
 
 		this.controllerState = new ControllerState();
 
-		this.gameManagementInputController = new GameManagementInputController(gameBus);
-
 		this.executorService.submit(controllerState);
 
 		this.gameBus.register(controllerState);
+		this.gameBus.register(this);
 
 		//for (Scene sceneLayer : sceneLayers) {
 		//	if (sceneLayer.getPickingShader() != null) {
@@ -72,7 +71,14 @@ public class GameLoop {
 		//	}
 		//}
 
+		directTransformController.setUserInput(controllerState);
+
+		GameManagementInputController gameManagementInputController = new GameManagementInputController(gameBus);
+		gameManagementInputController.setUserInput(controllerState);
+
 		geSystems.add(new TerrainGeneration());
+		geSystems.add(directTransformController);
+		geSystems.add(gameManagementInputController);
 
 	}
 
@@ -87,21 +93,20 @@ public class GameLoop {
 		try {
 			window.init(wip);
 		} catch (IOException e) {
-			e.printStackTrace();
+			gameBus.dispatch(new ErrorEvent(e, ErrorEventType.CRITICAL));
+			return;
 		}
 
 		while (!window.shouldClose()) {
 			long now = System.nanoTime();
-			deltaSeconds += (now - lastTime) / 1000000000.0;
 			window.render();
+			deltaSeconds += (now - lastTime) / 1000000000.0;
 			window.setTitle("FPS: " + 1 / deltaSeconds);
 			deltaSeconds = 0.0;
 			lastTime = now;
 		}
 
 		window.close();
-		shutdown = true;
-		executorService.shutdown();
 	}
 
 	public void update() {
@@ -119,9 +124,6 @@ public class GameLoop {
 				deltaSeconds += (now - lastTime) / 1000000000.0;
 
 				if (deltaSeconds >= 1 / FPS) {
-
-					directTransformController.updateUserInput(controllerState);
-					gameManagementInputController.updateUserInput(controllerState);
 
 					// update systems
 					for (GESystem geSystem : geSystems) {
@@ -172,4 +174,17 @@ public class GameLoop {
 		return executorService;
 	}
 
+	@Override
+	public void handle(Event<?> event) {
+
+		if (event.getType().equals(ManagementEventType.SHUTDOWN)) {
+			shutdown = true;
+			executorService.shutdown();
+		}
+	}
+
+	@Override
+	public boolean supports(Class<? extends Event> aClass) {
+		return supports.contains(aClass);
+	}
 }
